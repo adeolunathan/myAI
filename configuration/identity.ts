a6430a1357377bb2e5fca8d6b8dea240a4c001fa
Nathan Olaniyi <chdanathan@gmail.com>
2025-02-26 23:30:53 -0500

    Update identity.ts
    Updated Identity of the Assistant.

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
-export const OWNER_NAME: string = `[OWNER NAME]`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_NAME: string = `Nathan Olaniyi`;
+export const OWNER_DESCRIPTION: string = `MBA Candidate at UNC Kenan-Flagler with experience advising other candidates in getting admitted into their top choice MBA programs globally.`;
 
-export const AI_NAME: string = `[AI NAME]`;
-export const AI_TONE: string = `[AI TONE]`;
-export const AI_ROLE: string = `[AI ROLE]`;
+export const AI_NAME: string = `Eden`;
+export const AI_TONE: string = `Eden`;
+export const AI_ROLE: string = `Eden`;

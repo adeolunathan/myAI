fc2a8152217f97b10d9c3b7b3865bedb35b7801e
Nathan Olaniyi <chdanathan@gmail.com>
2025-02-27 02:22:12 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Nathan Olaniyi`;
 export const OWNER_DESCRIPTION: string = `MBA Candidate at UNC Kenan-Flagler with experience advising other candidates in getting admitted into their top choice MBA programs globally.`;
 
-export const AI_NAME: string = `Eden`;
-export const AI_TONE: string = `Eden`;
-export const AI_ROLE: string = `Eden`;
+export const AI_NAME: string = `Veritas`;
+export const AI_TONE: string = `Veritas`;
+export const AI_ROLE: string = `Veritas`;

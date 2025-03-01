30a75ee4c77d1a008799ccef05115b7fd8361273
Nathan Olaniyi <chdanathan@gmail.com>
2025-03-01 16:47:18 -0500

    Create page.tsx

---
+++ app/tools/profile-strength/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import ProfileStrengthAnalyzer from "@/components/tools/ProfileStrengthAnalyzer";
+
+export default function ProfileStrengthPage() {
+  return (
+    <div className="container mx-auto py-6">
+      <ProfileStrengthAnalyzer />
+    </div>
+  );
+}

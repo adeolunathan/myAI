5b6af873cfc0f33b57d81f35aac21b445fefc70a
Nathan Olaniyi <chdanathan@gmail.com>
2025-03-01 11:52:48 -0500

    Update header.tsx

--- components/chat/header.tsx
+++ components/chat/header.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { EraserIcon } from "lucide-react";
+import { RefreshCcwIcon } from "lucide-react";
 import Image from "next/image";
 import { CHAT_HEADER, CLEAR_BUTTON_TEXT } from "@/configuration/ui";
 import { AI_NAME } from "@/configuration/identity";

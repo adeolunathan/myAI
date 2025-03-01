989307d5541cc68a94e49803046edd7b5df3f4f8
Nathan Olaniyi <chdanathan@gmail.com>
2025-03-01 16:43:42 -0500

    Create page.tsx

---
+++ app/tools/school-comparison/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import SchoolComparison from "@/components/tools/SchoolComparison";
+
+export default function SchoolComparisonPage() {
+  return (
+    <div className="container mx-auto py-6">
+      <SchoolComparison />
+    </div>
+  );
+}
